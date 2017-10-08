422ecc91e3bbea0dcc9615399d7aa24ed4e98d7d
Tomek Lis <11555082+TomekLis@users.noreply.github.com>
2017-10-08 16:39:14 +0200

    samplle

--- MobileSchoolRegisterAppApi/MobileSchoolRegisterAppApi/Models/AccountBindingModels.cs
+++ MobileSchoolRegisterAppApi/MobileSchoolRegisterAppApi/Models/AccountBindingModels.cs
@@ -7,7 +7,7 @@ namespace MobileSchoolRegisterAppApi.Models
     // Models used as parameters to AccountController actions.
 
     public class AddExternalLoginBindingModel
-    {
+    {//asd
         [Required]
         [Display(Name = "External access token")]
         public string ExternalAccessToken { get; set; }
