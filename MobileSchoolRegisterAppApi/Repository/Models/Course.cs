2d2cc43384b2af0b44b3e0b3de4fd212e790e311
Tomek Lis <11555082+TomekLis@users.noreply.github.com>
2017-10-11 00:12:19 +0200

    Dodanie pierwszych klas modelu

---
+++ MobileSchoolRegisterAppApi/Repository/Models/Course.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Repository.Models
+{
+    public class Course
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string Room { get; set; }
+        public int TeacherId { get; set; }
+        public int StudentsGroupId { get; set; }
+        public virtual Teacher Teacher { get; set; }
+        public virtual StudentsGroup StudentsGroup { get; set; }
+    }
+}
---
+++ MobileSchoolRegisterAppApi/Repository/Models/Teacher.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Security.Claims;
+using System.Threading.Tasks;
+using System.Web;
+using Microsoft.AspNet.Identity;
+using Microsoft.AspNet.Identity.EntityFramework;
+
+namespace Repository.Models
+{
+    public class Teacher : IdentityUser
+    {
+        public Teacher()
+        {
+            this.Courses = new HashSet<Course>();
+        }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+
+        [NotMapped]
+        public string FullName => FirstName + " " + LastName;
+
+        public virtual ICollection<Course> Courses { get; set; }
+
+        public async Task<ClaimsIdentity> GenerateUserIdentityAsync(UserManager<Teacher> manager)
+        {
+            // Note the authenticationType must match the one defined in CookieAuthenticationOptions.AuthenticationType
+            var userIdentity = await manager.CreateIdentityAsync(this, DefaultAuthenticationTypes.ApplicationCookie);
+            // Add custom user claims here
+            return userIdentity;
+        }
+
+    }
+}
