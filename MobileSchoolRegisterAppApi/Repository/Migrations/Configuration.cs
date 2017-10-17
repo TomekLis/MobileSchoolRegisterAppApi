d90532e49f11029410c9deb8efacbe6b28d76a9f
Tomek Lis <11555082+TomekLis@users.noreply.github.com>
2017-10-17 23:15:20 +0200

    mały commit

--- MobileSchoolRegisterAppApi/Repository/Migrations/Configuration.cs
+++ MobileSchoolRegisterAppApi/Repository/Migrations/Configuration.cs
@@ -10,7 +10,7 @@ namespace Repository.Migrations
     using System.Data.Entity.Migrations;
     using System.Linq;
 
-    internal sealed class Configuration : DbMigrationsConfiguration<Models.Contexts.SchoolRegisterContext>
+    internal sealed class Configuration : DbMigrationsConfiguration<SchoolRegisterContext>
     {
         public Configuration()
         {
